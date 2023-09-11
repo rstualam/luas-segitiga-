f44a65c6c8812b9eefddf53c5d83505e16479097
rstualam <144640285+rstualam@users.noreply.github.com>
2023-09-11 09:28:14 +0700

    Create luas segitiga.cpp

---
+++ luas segitiga.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main () {
+	int a,t ;
+	cout << " MENGHITUNG LUAS SEGITIGA  \n";
+	cout <<"masukan alas : " ;
+	cin >> a ;
+	cout << "masukan tinggi : " ;
+	cin >> t ;
+	cout <<"Hasil Luas : "<< 0.5*a*t <<endl ;
+	return 0 ;
+}
